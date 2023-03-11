476e7538fc7c0cf52f63fbc51d51d3d469b083b0
Tony Owens <anthony.owens1987@gmail.com>
2023-03-11 10:24:37 -0800

    Added helpers to managed resource groups. Added helper to get terraform key names

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,9 +1,17 @@
 package helpers
 
 import (
+	"context"
 	"os"
 	"path"
 	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2021-04-01/resources"
+	"github.com/gruntwork-io/terratest/modules/azure"
+)
+
+var (
+	ctx = context.Background()
 )
 
 // Creates a file if does not already exist on the specified path
@@ -32,6 +40,28 @@ provider "azurerm" {
 	return err
 }
 
+func CreateAzureResourceGroup(t *testing.T, subscriptionID string, resourceGroup, string, location string) error {
+	resourceGroupClient, err := azure.CreateResourceGroupClientE(subscriptionID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create the resource group using the resourceGroupClient
+	resp, err := resourceGroupClient.CreateOrUpdate(ctx, resourceGroup, resources.Group{
+		Location: &location,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode == 201 {
+		t.Logf("Created resource group '%s'", *resp.Name)
+	}
+
+	return err
+}
+
 // Cleans up the common files created by terraform during init, plan, and apply
 func CleanUpTestFiles(t *testing.T, files []string, workDir string) error {
 	for _, file := range files {
@@ -43,4 +73,29 @@ func CleanUpTestFiles(t *testing.T, files []string, workDir string) error {
 	}
 
 	return nil
+}
+
+func DeleteAzureResourceGroup(t *testing.T, subscriptionID string, resourceGroup string) error {
+	resourceGroupClient, err := azure.CreateResourceGroupClientE(subscriptionID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = resourceGroupClient.Delete(ctx, resourceGroup, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return err
+}
+
+// Gets the keys of a terraform map and returns a slice of strings
+func GetMapKeys(t *testing.T, terraformMap map[string]any) []string {
+	keys := make([]string, len(terraformMap))
+	i := 0
+	for key := range terraformMap {
+		keys[i] = key
+		i++
+	}
+
+	return keys
 }
